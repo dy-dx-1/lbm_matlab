e4bbdaba49377f53dfd7656374c0924be339ed20
dy-dx-1 <nic19sarbe@gmail.com>
2024-04-12 18:39:31 -0400

    fixed streamlines

--- navier_stokes/verif_assets/show_shape.m
+++ navier_stokes/verif_assets/show_shape.m
@@ -1,3 +1,3 @@
 function show_shape(x_cyl, y_cyl, cyl_rad_nodes)
     % rectangle function is easiest to draw a circle, pos vector outlines lower left corner and height and width, curvature makes it a circle 
-    rectangle('Position', [x_cyl-cyl_rad_nodes y_cyl-cyl_rad_nodes-1 cyl_rad_nodes*2 cyl_rad_nodes*2], 'Curvature', [1 1], 'FaceColor', 'red')
+    rectangle('Position', [x_cyl-cyl_rad_nodes y_cyl-cyl_rad_nodes-1 cyl_rad_nodes*2 cyl_rad_nodes*2], 'Curvature', [1 1], 'FaceColor', 'white')
